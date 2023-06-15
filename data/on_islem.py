c06e4f816ee9ccc21879ee4d54513e725ab5b6a7
zubairnajim <zubairn2000@gmail.com>
2023-06-15 21:26:29 +0300

    commit -m "word2vec"

--- data/on_islem.py
+++ data/on_islem.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import re
 import snowballstemmer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from gensim.models import Word2Vec
 
 
 #Sayisal degerlerin yok edilmesi
@@ -17,7 +20,7 @@ def remove_emoji(value):
 
 #tek character 
 def remove_single_character(value):
-    return re.sub(r'(?:^|)\w(?:$)', '', value)
+    return re.sub(r'(?:^| )\w(?:$| )', '', value)
 
 #noktalama isaretlerin kaldirmasi
 def remove_noktalama(value):
@@ -37,7 +40,7 @@ def remove_username(value):
 
 #stop word function
 def stem_word(value):
-    stemmer = snowballstemmer.stemmer('turkish')
+    stemmer = snowballstemmer.stemmer("turkish")
     value = value.lower()
     value = stemmer.stemWords(value.split())
     stop_words = ['fakat','lakin','ancak','acaba', 'ama', 'aslında', 'az', 'bazı', 'belki', 'biri', 'birkaç',
@@ -46,7 +49,7 @@ def stem_word(value):
                    'nasıl', 'ne', 'neden', 'nerde', 'nerede', 'nereye', 'niçin', 'niye', 'o', 'sanki', 'şey', 
                    'siz', 'şu', 'tüm', 've', 'veya', 'ya', 'yani']
     value = [item for item in value if not item in stop_words]
-    value = ''.join(value)
+    value = ' '.join(value)
     return value
 
 #cagirmasi
@@ -58,8 +61,35 @@ def pre_processing(value):
                              (remove_hash
                               (remove_username
                                (stem_word(word))))))))
-                               for word in value.split()]
+                               for word in str(value).split()]
 
 #Bosluklari silmek
 def remove_space(value):
     return [item for item in value if item.strip()]
+
+#Bag of words
+def bag_of_words(value): 
+    vectorizer = CountVectorizer()
+    x = vectorizer.fit_transform(value)
+    return
+
+#tf-idf model
+def tfidf(value):
+    vectorizer = TfidfVectorizer()
+    x = vectorizer.fit_transform(value)
+    return x.toarray().tolist()
+
+#word2vec model
+def word2vec(value):
+    model = Word2Vec.load("X:\\EEM Dosyalar\\Yazan Zeka 4e Academy\\NLP Dogal Dil\\Natural-Language-Processing/data/word2vec.model")
+    bfr_list = []
+    bfr_len = len(value)
+
+    for k in value:
+        bfr = model.wv.key_to_index[k]
+        bfr = model.wv[bfr]
+        bfr_list.append(bfr)
+
+    bfr_list = sum(bfr_list)
+    bfr_list = bfr_list/bfr_len
+    return bfr_list.tolist()
